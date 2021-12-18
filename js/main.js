765ec163a8a0763cd0b8d4e220c01ef1affeef4c
Froilan <froilanmartinr@gmail.com>
2021-12-17 23:49:41 -0300

    agregando js a Blog y modificaciones

--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
 $(document).ready(() => {
     $('#verMas').hide();
+    $('#parrafosNota1Oculto').hide();
+    $('#parrafosNota2Oculto').hide();
 
-    $('#btn-ver').click(mostrarOcultarTexto);
+    $('#btn-ver').click(mostrarOcultarTextoInicio);
+    $('#btnNota1').click(mostrarOcultarTextoNota1);
+    $('#btnNota2').click(mostrarOcultarTextoNota2);
+
     let mostrar = true;
-    function mostrarOcultarTexto() {
+    function mostrarOcultarTextoInicio() {
         if (mostrar == true) {
             $('#btn-ver').text('Ver Menos');
             $('#verMas').show();
@@ -14,6 +19,32 @@ $(document).ready(() => {
             $('#btn-ver').text('Ver Más');
             mostrar = true;
         }
-
     }
+
+    function mostrarOcultarTextoNota1() {
+        if (mostrar == true) {
+            $('#btnNota1').text('Ocultar');
+            $('#parrafosNota1Oculto').show();
+            mostrar = false;
+        }
+        else {
+            $('#parrafosNota1Oculto').hide();
+            $('#btnNota1').text('Continuar leyendo');
+            mostrar = true;
+        }
+    }
+
+    function mostrarOcultarTextoNota2() {
+        if (mostrar == true) {
+            $('#btnNota2').text('Ocultar');
+            $('#parrafosNota2Oculto').show();
+            mostrar = false;
+        }
+        else {
+            $('#parrafosNota2Oculto').hide();
+            $('#btnNota2').text('Continuar leyendo');
+            mostrar = true;
+        }
+    }
+
 });
